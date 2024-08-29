4019be4364e64b1998f69f12de0293200bbcfb6a
Artem Sharovatov <git@sharovatov.me>
2024-08-29 17:36:49 +0300

    Move game files into a separate folder

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -5,6 +5,8 @@ task: str = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def generate_question_and_answer() -> tuple[str, str]:
-    number: int = random.randint(-999, 999)
+    range_min: int = -999
+    range_max: int = 999
+    number: int = random.randint(range_min, range_max)
     correct_answer: str = 'yes' if number % 2 == 0 else 'no'
     return str(number), correct_answer
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-import brain_games.brain_even
+import brain_games.games.brain_even
 import brain_games.game_engine
 
 
 def main():
     brain_games.game_engine.engine(
-        brain_games.brain_even.task,
-        brain_games.brain_even.generate_question_and_answer
+        brain_games.games.brain_even.task,
+        brain_games.games.brain_even.generate_question_and_answer
     )
 
 

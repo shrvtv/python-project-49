2e64e59af23aaaf7ff8cfbdb5b5c5a338b560777
Artem Sharovatov <git@sharovatov.me>
2024-08-29 17:02:53 +0300

    Refactor the code to avoid duplication by creating game engine

---
+++ brain_games/game_engine.py
@@ -0,0 +1,31 @@
+import prompt
+import typing
+
+
+def engine(
+    task: str,
+    generate_question_and_answer: typing.Callable[[], tuple[str, str]]
+) -> None:
+    print("Welcome to the Brain Games!")
+    name: str = prompt.string("May I have your name? ")
+    print(f'Hello, {name}!')
+    print(task)
+    successful_tries_left: int = 3
+    while successful_tries_left:
+        raw_question: str
+        correct_answer: str
+        raw_question, correct_answer = generate_question_and_answer()
+        print(f'Question: {raw_question}')
+        player_answer: str = prompt.string('Your answer: ')
+        if player_answer == correct_answer:
+            print('Correct!')
+            successful_tries_left -= 1
+            if successful_tries_left == 0:
+                print(f'Congratulations, {name}!')
+        else:
+            print(
+                f"'{player_answer}' is wrong answer ;(. "
+                f"Correct answer was '{correct_answer}'."
+            )
+            print(f"Let's try again, {name}")
+            break
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 import brain_games.brain_even
+import brain_games.game_engine
 
 
 def main():
-    brain_games.brain_even.game()
+    brain_games.game_engine.engine(
+        brain_games.brain_even.task,
+        brain_games.brain_even.generate_question_and_answer
+    )
 
 
 if __name__ == "__main__":
